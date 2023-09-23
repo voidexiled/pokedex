7b6b193b2688f59583dce81eb2f531195c0b7484
voidexiled <l20070518@cdmadero.tecnm.mx>
2023-09-22 18:41:16 -0600

    SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,18 +5,38 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Pokedex",
-  description: "Pokedex made with nextjs",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content={metadata.description} />
-        <title>{metadata.title}</title>
+        <title>Pokedex</title>
+        <meta name="title" content="Pokedex" />
+        <meta name="description" content="Pokedex made with nextjs" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://void-pokedex.vercel.app/" />
+        <meta property="og:title" content="Pokedex" />
+        <meta property="og:description" content="Pokedex made with nextjs" />
+        <meta
+          property="og:image"
+          content="https://jesusjalomo.vercel.app/1.png"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://void-pokedex.vercel.app/"
+        />
+        <meta property="twitter:title" content="Pokedex" />
+        <meta
+          property="twitter:description"
+          content="Pokedex made with nextjs"
+        />
+        <meta
+          property="twitter:image"
+          content="https://jesusjalomo.vercel.app/1.png"
+        />
         <link rel="icon" favicon={favicon} />
       </head>
 
